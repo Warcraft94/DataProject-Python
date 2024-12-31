25382f7d1549c8e075a55f0b49ba683710b9f65c
Aurélien Fauconnier <aurelien.fauconnier@edu.esiee.fr>
2024-12-31 18:34:40 +0100

    feat: header delimiter

--- src/components/header.py
+++ src/components/header.py
@@ -15,6 +15,7 @@ def get_header(years):
                     )
                 ]
             ),
+            html.Hr(),
             html.Div(
                 id="header-slider",
                 children=[
