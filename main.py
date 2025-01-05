9657015c96af87daebe05c7f78b03157fe0b4d99
Aurélien Fauconnier <aurelien.fauconnier@edu.esiee.fr>
2025-01-05 13:05:07 +0100

    feat: cleaning and deleting because of the new clean_data method

--- main.py
+++ main.py
@@ -1,52 +1,54 @@
-import plotly_express as px
 import dash
-import pandas as pd
+import pandas
 import os
 import json
-from dash import dcc, html
-from dash.dependencies import Input, Output
-from src.utils import clean_geojson
 from src.pages import SimpleDashboard
 from config import DASH_DEBUG_MODE
+from src.utils import clean_data, clean_geojson
 
-#
-# Data
-#
-def load_data():
+def load_data() -> tuple:
     """Récupère les fichiers de données
 
     Returns:
-        pd.Dataframe: Dataframe des données d'énergies et d'émissions de CO2 par pays
+        Dataframe: Dataframe des données d'énergies et d'émissions de CO2 par pays
         dict: Dictionnaire geojson des pays
     """
+
+## TODO utiliser une fonction commune ici (car déjà quasiment identique à src/utils/clean_data.py)
     # Récupère le chemin absolu du répertoire où le script s'exécute
     current_dir = os.path.dirname(__file__) 
     
-    # Construit le chemin d'accès au fichier geojson
+    # Construit le chemin d'accès au fichier geojson et au fichier de données csv
     geojson_path = os.path.join(current_dir, "data/cleaned/cleaned_countries.geojson")
+    data_path = os.path.join(current_dir, "data/cleaned/cleaned_energy.csv")
+
     # Récupère le fichier geojson sous forme de dict()
     with open(geojson_path, "r") as f:
         geojson_data = json.load(f)
-        
-    # Construit le chemin d'accès au fichier csv
-    data_path = os.path.join(current_dir, "data/cleaned/formatted_energy.csv")
-    energy_data = pd.read_csv(data_path, sep=';')
     
-    energy_data["CO2_emission"] = pd.to_numeric(energy_data["CO2_emission"])
-    energy_data["Energy_consumption"] = pd.to_numeric(energy_data["Energy_consumption"].replace({',': '.'}, regex=True))
-    # energy_data["Energy_production"] = pd.to_numeric(energy_data["Energy_production"].replace({',': '.'}, regex=True))
-    
+    # Récupère le fichier de données sous forme de dataframe
+    energy_data = pandas.read_csv(data_path, sep=';')
+
     return energy_data, geojson_data
 
 #
 # Main
 #
 if __name__ == '__main__':
-    # Récupère les données
+
+    # Nettoie les fichiers de données et de geojson suivant les règles définies et les données nécessaires pour l'application
+    # print("Nettoyage des données geojson...")
+    # clean_geojson()
+    # print("Nettoyage des données csv...")
+    # clean_data()
+
+    # Récupère les données d'énergie et de géojson utilisées par l'application
     energy_data, geojson_data = load_data()
 
     # Créer l'instance de la classe SimpleDashboard
     dashboard_page = SimpleDashboard(energy_data, geojson_data)
+
+    print("Lancement de l'application web...")
 
     # Initialisation de l'application Dash
     app = dash.Dash(__name__)
@@ -62,4 +64,3 @@ if __name__ == '__main__':
 
     # Lance l'application web dash
     app.run_server(debug=DASH_DEBUG_MODE)
-    
